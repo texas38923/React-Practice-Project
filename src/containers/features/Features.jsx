e6f82d2c8648f0b137dcf5052a16f907fa30e803
Anurag Kamra <anuragkamra7@gmail.com>
2024-01-05 16:10:17 +0530

    completed features and wgpt3

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,8 +1,44 @@
 import React from 'react';
 import './features.css';
+import { Feature } from '../../components';
+
+const featuresData = [
+  {
+    title: 'Lorem, ipsum dolor',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta explicabo facere enim magnam aperiam expedita quod dolores quis sequi perspiciatis dolorem quibusdam, rem non quidem?',
+  },
+  {
+    title: 'Lorem, ipsum dolor',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta explicabo facere enim magnam aperiam expedita quod dolores quis sequi perspiciatis dolorem quibusdam, rem non quidem?',
+  },
+  {
+    title: 'Lorem, ipsum dolor',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta explicabo facere enim magnam aperiam expedita quod dolores quis sequi perspiciatis dolorem quibusdam, rem non quidem?',
+  },
+  {
+    title: 'Lorem, ipsum dolor',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta explicabo facere enim magnam aperiam expedita quod dolores quis sequi perspiciatis dolorem quibusdam, rem non quidem?',
+  },
+];
 
 const Features = () => {
-  return <div></div>;
+  return (
+    <div className='gpt3__features section__padding' id='features'>
+      <div className='gpt3__features-heading'>
+        <h1 className='gradient__text'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+          dolorum aperiam voluptatibus?
+        </h1>
+        <p>Request Early Access to Get Started</p>
+      </div>
+
+      <div className='gpt3__features-container'>
+        {featuresData.map((item, ind) => (
+          <Feature title={item.title} text={item.text} key={item.title + ind} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Features;
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import './possibility.css';
+import PossibilityImage from '../../assets/possibility.png';
 
 const Possibility = () => {
-  return <div></div>;
+  return (
+    <div className='gpt3__possibility section__padding' id='possibility'>
+      <div className='gpt3__possibility-image'>
+        <img src={PossibilityImage} alt='possibility' />
+      </div>
+
+      <div className='gpt3__possibility-content'>
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className='gradient__text'>
+          The possibilities are beyond our imagination
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt eum
+          harum, natus quos nemo, obcaecati reprehenderit at fuga modi hic,
+          exercitationem a optio minus beatae!
+        </p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
+    </div>
+  );
 };
 
 export default Possibility;
