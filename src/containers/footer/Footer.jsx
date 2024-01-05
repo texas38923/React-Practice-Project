7f3680fca42b1331c23f415621a0cd4f877bcde9
Anurag Kamra <anuragkamra7@gmail.com>
2024-01-05 18:08:23 +0530

    completed the project

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,8 +1,54 @@
 import React from 'react';
 import './footer.css';
+import gpt3Logo from '../../assets/logo.svg';
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className='gpt3__footer section__padding'>
+      <div className='gpt3__footer-heading'>
+        <h1 className='gradient__text'>
+          Do you want to step into the future before others?
+        </h1>
+      </div>
+
+      <div className='gpt3__footer-btn'>
+        <p>Request Early Access</p>
+      </div>
+
+      <div className='gpt3__footer-links'>
+        <div className='gpt3__footer-links_logo'>
+          <img src={gpt3Logo} alt='logo' />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
+            et? , All Rights Reserved.
+          </p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Get it Touch</h4>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>9351441265</p>
+          <p>gpt3@gptmail.com</p>
+        </div>
+      </div>
+
+      <div className='gpt3__footer-copyright'>
+        <p>All rights Reserved.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
